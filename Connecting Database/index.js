5e75adc566e3712e2573b074069a335d011aa3f2
Ashish Raghav <ashishraghav2468@gmail.com>
2024-06-19 12:54:32 +0530

    database connected

---
+++ Connecting Database/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://ashishraghav2468:WQi5dE1ChYd5iIAf@cluster0.jlpnbag.mongodb.net/userappnew",
+);
+const User = mongoose.model('Users',{name: String,email: String,password: String});
+app.post("/signup", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const existinguser = User.findOne({email:email});
+  if(existinguser){
+    return res.status(400).send("User already exists");
+  }
+  const user = new User({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  user.save();
+  res.json({
+    "msg":"User created successfully",
+  })
+});
+
+app.listen(3000);
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const mongoose = require("mongoose");
+// const jwtPassword = "123456";
+
+// mongoose.connect(
+//   "your_mongo_url",
+// );
+
+// const User = mongoose.model("User", {
+//   name: String,
+//   username: String,
+//   pasword: String,
+// });
+
+// const app = express();
+// app.use(express.json());
+
+// function userExists(username, password) {
+//   // should check in the database
+// }
+
+// app.post("/signin", async function (req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   if (!userExists(username, password)) {
+//     return res.status(403).json({
+//       msg: "User doesnt exist in our in memory db",
+//     });
+//   }
+
+//   var token = jwt.sign({ username: username }, "shhhhh");
+//   return res.json({
+//     token,
+//   });
+// });
+
+// app.get("/users", function (req, res) {
+//   const token = req.headers.authorization;
+//   try {
+//     const decoded = jwt.verify(token, jwtPassword);
+//     const username = decoded.username;
+//     // return a list of users other than this username from the database
+//   } catch (err) {
+//     return res.status(403).json({
+//       msg: "Invalid token",
+//     });
+//   }
+// });
+
+// app.listen(3000);
